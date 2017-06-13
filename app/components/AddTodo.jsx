50fd2605b0285ac65a6ef7b4dae2b9046052556b
shansen5 <steven.e.hansen@gmail.com>
2017-06-12 22:29:39 -0700

    Create AddTodo component.

---
+++ app/components/AddTodo.jsx
@@ -0,0 +1,30 @@
+var React = require( 'react' );
+
+var AddTodo = React.createClass( {
+    onFormSubmit: function (e) {
+        e.preventDefault();
+        var todoText = this.refs.todoText.value;
+        if ( todoText && todoText.trim().length > 0 ) {
+            this.props.onAddTodo( todoText );
+            this.refs.todoText.value = '';
+        } else {
+            this.refs.todoText.focus();
+        }
+    },
+    render: function () {
+        return( 
+            <div>
+                <form ref="form" onSubmit={this.onFormSubmit} className="addtodo">
+                    <div>
+                        <input type="text" placeholder="Enter something you need to do" ref="todoText"/>
+                    </div>
+                    <div>
+                        <button className="button expanded">Add Todo</button>
+                    </div>
+                </form>
+            </div>
+        )
+    }
+})
+
+module.exports = AddTodo;
---
+++ app/tests/components/AddTodo.test.jsx
@@ -0,0 +1,31 @@
+var expect = require( 'expect' );
+var React = require( 'react' );
+var ReactDOM = require( 'react-dom' );
+var TestUtils = require( 'react-addons-test-utils' );
+var $ = require( 'jquery' );
+
+var AddTodo = require( 'AddTodo' );
+
+describe( 'AddTodo', () => {
+    it( 'should exist', () => {
+        expect( AddTodo ).toExist();
+    });
+    it( 'should call onAddTodo if valid string entered', () => {
+        var spy = expect.createSpy();
+        var addTodo = TestUtils.renderIntoDocument(
+            <AddTodo onAddTodo={spy}/> );
+        var $el = $( ReactDOM.findDOMNode( addTodo ));
+        addTodo.refs.todoText.value = 'Play guitar';
+        TestUtils.Simulate.submit( $el.find( 'form' )[0] );
+        expect( spy ).toHaveBeenCalledWith( 'Play guitar' );
+    });
+    it( 'should not call onAddTodo if blank string entered', () => {
+        var spy = expect.createSpy();
+        var addTodo = TestUtils.renderIntoDocument(
+            <AddTodo onAddTodo={spy}/> );
+        var $el = $( ReactDOM.findDOMNode( addTodo ));
+        addTodo.refs.todoText.value = '   ';
+        TestUtils.Simulate.submit( $el.find( 'form' )[0] );
+        expect( spy ).toNotHaveBeenCalled();
+    });
+});
